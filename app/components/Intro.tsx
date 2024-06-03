d0d0bd43f2a1e58161ff4d3ead29d3f29a4415cf
mukhil0212 <138153252+mukhil0212@users.noreply.github.com>
2024-06-03 15:14:09 -0400

    email changes

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -64,7 +64,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Mukhil.</span> I'm a{" "}
+        <span className="font-bold">Hello I'm Mukhil,</span> A{" "}
         <span className="font-bold">rising senior</span> in Computer Science at{" "}
         <span className="font-bold">Georgia State University</span>, graduating{" "}
         in <span className="italic">Summer 2025</span>.
