00e586a9b4ae2313637f7a360913538ac1865c6b
mukhil0212 <138153252+mukhil0212@users.noreply.github.com>
2024-06-05 19:34:11 -0400

    made stars interactive

--- app/components/StarBackground.tsx
+++ app/components/StarBackground.tsx
@@ -1,20 +1,39 @@
 "use client";
-
-import React, { useState, useRef, Suspense } from "react";
+import React, { useState, useRef, useEffect, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 // @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
 const StarBackground = (props: any) => {
-  const ref: any = useRef();
+  const ref = useRef<any>();
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  // Event listener to update mouse position
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      setMousePosition({
+        x: (event.clientX / window.innerWidth) * 2 - 1, // Normalize the mouse x
+        y: 0, // Remove vertical movement
+      });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const rotationFactor = 0.7; // Reduced sensitivity for slower rotation
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
+    if (ref.current) {
+      // Update rotation based on normalized mouse coordinates
+      ref.current.rotation.y += mousePosition.x * rotationFactor * delta;
+    }
   });
 
   return (
@@ -22,10 +41,10 @@ const StarBackground = (props: any) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color="$fff"
+          color="#fff"
           size={0.002}
           sizeAttenuation={true}
-          dethWrite={false}
+          depthWrite={false}
         />
       </Points>
     </group>
